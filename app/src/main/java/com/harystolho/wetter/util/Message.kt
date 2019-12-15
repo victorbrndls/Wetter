63911b69c2b92f08c087c5641794ea0377f15922
Harystolho <haristolhogamer@gmail.com>
2019-12-15 15:41:38 -0300

    Show error messages when something goes wrong

---
+++ app/src/main/java/com/harystolho/wetter/util/Message.kt
@@ -0,0 +1,20 @@
+package com.harystolho.wetter.util
+
+import android.content.Context
+import com.harystolho.wetter.R
+
+interface Message {
+    fun getMessage(context: Context): String
+}
+
+class UnknownError : Message {
+    override fun getMessage(context: Context): String {
+        return context.getString(R.string.error_message_unknown)
+    }
+}
+
+class NoInternetError : Message {
+    override fun getMessage(context: Context): String {
+        return context.getString(R.string.error_message_no_internet)
+    }
+}
