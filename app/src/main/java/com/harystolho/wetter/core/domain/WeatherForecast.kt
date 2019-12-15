af92c9c93f422380013af0fb02b030474c253b96
Harystolho <haristolhogamer@gmail.com>
2019-12-15 09:00:36 -0300

    Add WeatherForecast to hold forecast data

---
+++ app/src/main/java/com/harystolho/wetter/core/domain/WeatherForecast.kt
@@ -0,0 +1,33 @@
+package com.harystolho.wetter.core.domain
+
+import java.util.*
+
+data class WeatherForecast(
+    val cityId: Int,
+    val cityName: String,
+
+    val forecastDate: Calendar,
+
+    val maxTemp: Double,
+    val minTemp: Double,
+
+    // "Feels like" max temperature
+    val appMaxTemp: Double,
+    // "Feels like" min temperature
+    val appMinTem: Double,
+
+    // meters / second
+    val windSpeed: Double,
+    val windDirection: String,
+
+    val timezone: String,
+
+    // a value between 0 and 1
+    val precipitation: Double,
+
+    // a value between 0 and 1
+    val relativeHumidity: Double,
+
+    // a value between 0 and 1
+    val cloudsPercentage: Double
+)
